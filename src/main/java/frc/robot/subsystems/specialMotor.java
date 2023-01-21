f993f0bfd53b5ab755a30bb6074ab4a153620750
lethalsandwich <mcjenseneverett@gmail.com>
2023-01-21 13:54:33 -0500

    special stuff

---
+++ src/main/java/frc/robot/subsystems/specialMotor.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol
+
+import 
---
+++ src/main/java/frc/robot/subsystems/specialServos.java
@@ -0,0 +1,10 @@
+/* package frc.robot.subsystems;
+
+import frc.robot.Constants;
+
+Servo leftServo = new servo(1);
+Servo rightServo = new servo(1);
+
+leftServo.setAngle(30);
+rightServo.setAngle(30); /*
+
