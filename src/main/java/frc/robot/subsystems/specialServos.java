23c68b817df96330e7ecf61b754f4b730c02d765
Lethalsandwich <122191113+Lethalsandwich@users.noreply.github.com>
2023-01-22 23:38:40 -0500

    Update and rename src/main/java/frc/robot/subsystems/specialServos.java to src/main/java/frc/robot/commands/retractArm.java

--- src/main/java/frc/robot/subsystems/specialServos.java
+++
@@ -1,9 +0,0 @@
-// package frc.robot.subsystems;
-
-// import frc.robot.Constants;
-
-// Servo leftServo = new servo(1);
-// Servo rightServo = new servo(1);
-
-// leftServo.setAngle(30);
-// rightServo.setAngle(30);
