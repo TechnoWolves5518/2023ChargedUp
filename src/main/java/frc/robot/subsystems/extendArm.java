a03091e4e8566d260aa15c4dba160d40716d333b
Lethalsandwich <122191113+Lethalsandwich@users.noreply.github.com>
2023-01-22 23:35:05 -0500

    Update and rename specialMotor.java to extendArm.java

---
+++ src/main/java/frc/robot/subsystems/extendArm.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj.motorcontrol;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+
+public class extendArm extends InstantCommand {
+    
+  
+}
