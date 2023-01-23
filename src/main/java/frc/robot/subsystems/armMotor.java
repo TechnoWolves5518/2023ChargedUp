3e3f4489f786bfeee535d06cd2cbbc76e6b5bf0b
Lethalsandwich <mcjenseneverett@gmail.com>
2023-01-23 00:14:28 -0500

    fixed stuff, added files.

--- src/main/java/frc/robot/subsystems/armMotor.java
+++ src/main/java/frc/robot/subsystems/armMotor.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 
-public class extendArm extends SubsystemBase {
+public class armMotor extends SubsystemBase {
     
   
 }
