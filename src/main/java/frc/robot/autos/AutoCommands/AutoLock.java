b33c7437e5a5d3d0ab2ed014203f40206fa1f08d
Kevin Phan <43834781+supermario6409@users.noreply.github.com>
2023-02-02 19:55:16 -0500

    drivebase lock works

--- src/main/java/frc/robot/autos/AutoCommands/AutoLock.java
+++ src/main/java/frc/robot/autos/AutoCommands/AutoLock.java
@@ -1,4 +1,4 @@
-package frc.robot.commands;
+package frc.robot.autos.AutoCommands;
 
 import frc.robot.Constants;
 import frc.robot.Constants.SwerveDrive;
@@ -7,22 +7,21 @@ import frc.robot.subsystems.Swerve;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
-
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 
-public class TeleopSwerve extends CommandBase {    
+public class AutoLock extends CommandBase {    
     private Swerve s_Swerve;    
+    //delete these "unused variables" if you want the code to break
     private DoubleSupplier translationSup;
     private DoubleSupplier strafeSup;
     private DoubleSupplier rotationSup;
     private BooleanSupplier robotCentricSup;
     private boolean brakeCheck;
     private final XboxController driver = new XboxController(0);
-    public TeleopSwerve(Swerve s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
+    public AutoLock(Swerve s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
         this.s_Swerve = s_Swerve;
         addRequirements(s_Swerve);
 
@@ -30,22 +29,15 @@ public class TeleopSwerve extends CommandBase {
         this.strafeSup = strafeSup;
         this.rotationSup = rotationSup;
         this.robotCentricSup = robotCentricSup;
+        
     }
 
     @Override
     public void execute() {
         /* Get Values, Deadband*/
-        double translationVal = MathUtil.applyDeadband(translationSup.getAsDouble() * Constants.SwerveDrive.speedMod, Constants.stickDeadband);
-        double strafeVal = MathUtil.applyDeadband(strafeSup.getAsDouble() * Constants.SwerveDrive.speedMod, Constants.stickDeadband);
-        double rotationVal = MathUtil.applyDeadband(rotationSup.getAsDouble() * Constants.SwerveDrive.speedMod, Constants.stickDeadband);
-        brakeCheck = driver.getXButton();
-        System.out.println(brakeCheck);
-        /* Drive */
-        if (brakeCheck == true) {
-            translationVal = 0;
-            strafeVal = 0;
-            rotationVal = 0.05;
-        }
+        double translationVal = 0;
+        double strafeVal = 0;
+        double rotationVal = 0.05;
         s_Swerve.drive(
             new Translation2d(translationVal, strafeVal).times(SwerveDrive.maxSpeed), 
             rotationVal * Constants.SwerveDrive.maxAngularVelocity, 
@@ -53,4 +45,10 @@ public class TeleopSwerve extends CommandBase {
             true);
         
     }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
 }
